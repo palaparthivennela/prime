3f729e030e416b18b2049540bbccd95c41f8b8f6
palaparthivennela <69576559+palaparthivennela@users.noreply.github.com>
2021-05-12 10:07:56 +0530

    Create prime2.cpp

---
+++ prime2.cpp
@@ -0,0 +1,26 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0,i;
+    cin>>n;
+    for (i=2;i<sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+            count=count+1;
+
+        }
+    }
+
+      if (count==0)
+       {
+          cout<<"prime";
+       }
+       else
+       {
+         cout<<"not prime";
+       }
+       return 0;
+}
